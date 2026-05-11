43e2827cadb4b3cfe497f2572394e5c30396926b
Cody Gerrard <cody.gerrard@wsu.edu>
2026-05-11 14:55:32 -0700

    Add LeafNode subclass and tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,7 @@
+from re import L
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
@@ -37,6 +38,24 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode("p", "Hello")
         with self.assertRaises(NotImplementedError):
             node.to_html()
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html_basic(self):
+        node = LeafNode("p", "Hello, World!")
+        self.assertEqual(node.to_html(), "<p>Hello, World!</p>")
+
+    def test_to_html_with_props(self):
+        node = LeafNode("a", "click me!", {"href": "https://boot.dev"})
+        self.assertEqual(node.to_html(), '<a href="https://boot.dev">click me!</a>')
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "just some text")
+        self.assertEqual(node.to_html(), "just some text")
+
+    def test_repr(self):
+        node = LeafNode("p", "hello", {"class": "greeting"})
+        self.assertEqual(repr(node), "LeafNode(p, hello, {'class': 'greeting'})",)
+
 
 if __name__ == "__main__":
     unittest.main()
